0133f105bac98f6461fa6f49be8605eaf303d108
Kidochi31 <jasonmichaelsinclair@gmail.com>
2025-07-10 16:47:45 +1000

    Implemented packet create and interpret

--- packettest.py
+++ packettest.py
@@ -1,71 +1,31 @@
-from packet import PacketType, create_packet, interpret_packet
+from packet import PacketType, create_accept_packet, create_data_packet, create_request_packet, interpret_packet
 from random import randint
-from typing import Any
 from collections.abc import Callable
 
 def test_request() -> bool:
-    random_names = ["alice", "bob", "charlie", "david eric III", "archduke fredrick ferdinand II of austria"]
-    
     type = PacketType.REQUEST
-    vers = randint(0, 255)
-    version = randint(0, 65535)
-    name = random_names[randint(0, len(random_names) - 1)]
-    data = b"request packet"
-
-    packet_data: dict[str, Any] = {"vers": vers, "version": version, "name": name, "data": data}
-    return test_packet(packet_data, type)
+    version = randint(0, 255)
+    packet = create_request_packet(version)
+    return test_packet((type, version, None), packet)
 
 def test_accept() -> bool:
     type = PacketType.ACCEPT
-    vers = randint(0, 255)
-    version = randint(0, 65535)
-    timestep0 = randint(0, 2 ** 64 - 1)
-    lentimestep = randint(0, 65535)
-    timestamp = randint(0, 2**32 - 1)
-    data = b"accept packet"
-
-    packet_data: dict[str, Any] = {"vers": vers, "version": version, "timestep0": timestep0, "lentimestep": lentimestep, "timestamp": timestamp, "data": data}
-    return test_packet(packet_data, type)
+    version = randint(0, 255)
+    packet = create_accept_packet(version)
+    return test_packet((type, version, None), packet)
 
 def test_data() -> bool:
     type = PacketType.DATA
-    timestep = randint(0, 2 ** 24 -1)
-    timestamp = randint(0, 65535)
-    delay = randint(- 2 ** 15, 2 ** 15 - 1)
-    seg = randint(0, 65535)
-    totalsegs = randint(0, 65535)
-    data = b"data packet"
+    ack = randint(0, 2 ** 24 - 1)
+    data_options: list[tuple[int, bytes] | None] = [None, (0, b'HI'), (1, b'HELLO'), (1000, b'HEY'), (2 ** 24 - 1, b'KONNICHIWA')]
+    data = data_options[randint(0, len(data_options) - 1)]
+    packet = create_data_packet(ack, data)
+    return test_packet((type, ack, data), packet)
 
-    packet_data: dict[str, Any] = {"timestep": timestep, "timestamp": timestamp, "delay": delay, "seg": seg, "totalsegs": totalsegs, "data": data}
-    return test_packet(packet_data, type)
 
-def test_nak() -> bool:
-    type = PacketType.NAK
-    timestep = randint(0, 2 ** 24 -1)
-    num_segs = randint(0, 20)
-    segs: list[int] = []
-    for _ in range(num_segs):
-        segs.append(randint(0, 65535))
-
-    packet_data: dict[str, Any] = {"timestep": timestep, "segs": segs}
-    return test_packet(packet_data, type)
-
-def test_packet(data: dict[str, Any], type: PacketType) -> bool:
-    try:
-        packet = create_packet(type, data)
-        int_type, int_data = interpret_packet(packet)
-
-        try:
-            assert data == int_data
-            assert type == int_type
-            return True
-        except:
-            print(f"test failed for packet of type {type} (found {int_type})")
-            print(f"original packet: {data}")
-            print(f"interpreted packet: {int_data}")
-            return False
-    except:
-        return False
+def test_packet(expected: tuple[PacketType, int, tuple[int, bytes] | None] | None, packet: bytes) -> bool:
+    result = interpret_packet(packet)
+    return result == expected
 
 def test_number(amount: int, name: str, test: Callable[[], bool]):
     print("")
@@ -87,7 +47,6 @@ def main():
     test_number(100, "Request Packet", test_request)
     test_number(100, "Accept Packet", test_accept)
     test_number(100, "Data Packet", test_data)
-    test_number(100, "NAK Packet", test_nak)
 
     print("")
     print("-------------Finished All Packet Tests-------------")
