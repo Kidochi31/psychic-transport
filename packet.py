bd13b2a92aaf5c82457244d4d8c9ef294b7030cf
Kidochi31 <jasonmichaelsinclair@gmail.com>
2025-07-10 15:45:32 +1000

    commit

---
+++ packet.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+class PacketType(Enum):
+    REQUEST = 0b10000000
+    ACCEPT = 0b01000000
+    DATA = 0b00100000
+
+def create_request_packet(version: int) -> bytes:
+    type_bytes = PacketType.REQUEST.value.to_bytes(1, 'big')
+    version_bytes = version.to_bytes(1, 'big')
+    return type_bytes + version_bytes
+
+def create_accept_packet(version: int) -> bytes:
+    type_bytes = PacketType.ACCEPT.value.to_bytes(1, 'big')
+    version_bytes = version.to_bytes(1, 'big')
+    return type_bytes + version_bytes
+
+def create_data_packet(ack: int, message: tuple[int, bytes] | None) -> bytes:
+    data = PacketType.DATA.value.to_bytes(1, 'big')
+    data += ack.to_bytes(3, 'big')
+    if message is not None:
+        data += message[0].to_bytes(3, 'big')
+        data += message[1]
+    return data
+
+def interpret_packet(packet: bytes) -> tuple[PacketType, int, tuple[int, bytes] | None] | None:
+    # returns the packet type, acknowledgement/version, and data (if there is data)
+    if len(packet) < 2:
+        return None
+    if not packet[0] in [type.value for type in PacketType]:
+        return None
+    type = PacketType(packet[0])
+    match type:
+        case PacketType.REQUEST:
+            version = packet[1]
+            return (type, version, None)
+        case PacketType.ACCEPT:
+            version = packet[1]
+            return (type, version, None)
+        case PacketType.DATA:
+            if len(packet) < 4:
+                return None
+            acknowledgement = int.from_bytes(packet[1:4], 'big')
+            if len(packet) == 4:
+                return (type, acknowledgement, None)
+            # if it is greater, expect message number
+            if len(packet) < 7:
+                return None
+            message_number = int.from_bytes(packet[4:7], 'big')
+            return (type, acknowledgement, (message_number, packet[7:]))
